d6edfc06e665f7d5bcc929af4d4e9fafcbd1058e
Maurice Brown <mauricebrwn6890@gmail.com>
2022-11-03 18:32:56 -0500

    conditional

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,6 +1,6 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = 'Maurice';
+let firstName = 'Maurice ';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 let lastName = 'Brown';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
@@ -16,21 +16,25 @@ let luckynumber = 8
 let sentence = 'my name is, ' + fullname + ' and i think ' + luckynumber + ' is a winner!';
 console.log( sentence );
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-
+let adventurous = 'true';
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
-
+let food = 'fried chicken';
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-
+let pets = '0';
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-
+let friendsPets = '2';
 // 11 - Add two pets to your `pets` variable
-
+pets += 2;
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
+const allowedPets = '5';
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
-
-
+if (adventurous=true) {
+    console.log('Adventures are great!');
+}
+else if (adventurous=false){
+    console.log('How about we stay home?')
+}
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
 
